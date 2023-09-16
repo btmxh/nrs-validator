8028ddb3425f5259a2c385dfa16a0a06b31ebb76
btmxh <ngoduyanh.chip@gmail.com>
2023-09-16 23:31:09 +0700

    fix: :ambulance: map to filter
    wtf is wrong with me lmfao

--- validator/rules/dah-forbid-generic-types.ts
+++ validator/rules/dah-forbid-generic-types.ts
@@ -3,13 +3,13 @@ import { rule } from "../lib.ts";
 
 export default rule("dah-forbid-generic-types", function () {
   this.allEntries()
-    .map(
+    .filter(
       (entry) =>
         entry.DAH_meta.DAH_entry_type === StandardEntryType.LightNovelGeneric
     )
     .forEach((id) => this.warn(`Entry with LightNovelGeneric type: '${id}'`));
   this.allEntries()
-    .map(
+    .filter(
       (entry) =>
         entry.DAH_meta.DAH_entry_type === StandardEntryType.MusicGeneric
     )
