15b894c714404a0cd5d240f2d9ba2b949f600dbd
btmxh <ngoduyanh.chip@gmail.com>
2024-11-30 18:10:21 +0700

    deps: :arrow_up: upgrade nrs lib to 2.1.1

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export * from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v2.1.0/mod.ts";
-export * from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v2.1.0/exts/DAH_serialize_json.ts";
-export * from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v2.1.0/exts/DAH_standards.ts";
-export * from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v2.1.0/exts/DAH_entry_type.ts";
+export * from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v2.1.1/mod.ts";
+export * from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v2.1.1/exts/DAH_serialize_json.ts";
+export * from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v2.1.1/exts/DAH_standards.ts";
+export * from "https://raw.githubusercontent.com/ngoduyanh/nrs-lib-ts/v2.1.1/exts/DAH_entry_type.ts";
 
 export {
   type WrappedIterator,
